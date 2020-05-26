fca8dc43132d3edc1c78421392bf275f8641118a
John Doe <john@sample.com>
2020-05-26 09:53:54 +0800

    测试框架

--- workspace/seleniumFarmeWork/src/main/java/com/lpf/driver/SeleniumDriver.java
+++ workspace/seleniumFarmeWork/src/main/java/com/lpf/driver/SeleniumDriver.java
@@ -11,7 +11,7 @@ public class SeleniumDriver {
     public static WebDriver open(String browser){
         String path=System.getProperty("user.dir");
         if(browser.equals("chrome")){
-            System.setProperty("webdriver.chrome.driver",path+"/driver/chromedriver1.exe");
+            System.setProperty("webdriver.chrome.driver",path+"/driver/chromedriver.exe");
             driver=new ChromeDriver();
         }else if(browser.equals("ie")){
             System.setProperty("webdriver.ie.driver",path+"/driver/IEDriverServer.exe");
@@ -20,7 +20,7 @@ public class SeleniumDriver {
             System.setProperty("webdriver.gecko.driver",path+"/driver/geckodriver.exe");
             driver=new FirefoxDriver();
         }else if(browser.equals("edge")){
-            System.setProperty("webdriver.edge.driver",path+"/driver/msedgedriver1.exe");
+            System.setProperty("webdriver.edge.driver",path+"/driver/msedgedriver.exe");
             driver=new EdgeDriver();
         }else{
             System.err.println("您输入有误:"+browser);
